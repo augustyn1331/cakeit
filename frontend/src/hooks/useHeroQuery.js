7eb08da535eb31da65edbdd8d9a0b936cae76b0b
augustyn1331 <augustyn1331@gmail.com>
2021-10-20 16:46:21 +0200

    heroquery

---
+++ frontend/src/hooks/useHeroQuery.js
@@ -0,0 +1,21 @@
+import { useStaticQuery, graphql } from "gatsby";
+
+export const useHeroQuery = () => {
+  return useStaticQuery(graphql`
+    query HeroQuery {
+      wpPage(databaseId: { eq: 47 }) {
+        id
+        ACF_HomePage {
+          heroText
+          heroImage {
+            localFile {
+              childImageSharp {
+                gatsbyImageData(width: 1920, placeholder: TRACED_SVG)
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+};
