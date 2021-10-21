ff1367427b79630dd01a411c9ce0fa805efc2fe9
augustyn1331 <augustyn1331@gmail.com>
2021-10-21 16:37:57 +0200

    archive.js and gatsby-node config

--- frontend/gatsby-node.js
+++ frontend/gatsby-node.js
@@ -5,3 +5,59 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require("path");
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions;
+  const archiveTemplate = path.resolve("./src/templates/achive.js");
+  const result = await graphql(`
+    {
+      wp {
+        readingSettings {
+          postsPerPage
+        }
+      }
+      allWpCategory {
+        edges {
+          node {
+            id
+            name
+            count
+            uri
+            slug
+          }
+        }
+      }
+    }
+  `);
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`);
+    return;
+  }
+  const { allWpCategory, wp } = result.data;
+
+  allWpCategory.edges.forEach(category => {
+    const postsPerPage = wp.readingSettings.postsPerPage;
+    const { count: numberOfPosts, name, id, uri } = category.node;
+    const numberOfPages = Math.ceil(numberOfPosts / postsPerPage);
+    if (numberOfPosts > 0 || name !== "uncategorized") {
+      Array.from({ length: numberOfPages }).forEach((_, i) => {
+        createPage({
+          path: i === 0 ? `${uri}` : `${uri}${i + 1}`,
+          component: archiveTemplate,
+          context: {
+            limit: postsPerPage,
+            skip: i * postsPerPage,
+            numberOfPages,
+            currentPage: i + 1,
+            catId: id,
+            catName: name,
+            catUri: uri,
+            categories: allWpCategory,
+          },
+        });
+      });
+    }
+  });
+};
---
+++ frontend/src/templates/achive.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link, graphql } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import Layout from "../components/Layout/Layout";
+import BreadCrumb from "../components/BreadCrumb/BreadCrumb";
+
+import {
+  Wrapper,
+  ContentWrapper,
+  PageContent,
+  StyledH2,
+  StyledDate,
+  StyledReadMore,
+} from "./archive.styles";
+
+const archiveTemplate = () => <Layout>Archive</Layout>;
+
+export default archiveTemplate;
+
+// export const pageQuery = graphql`
+//   query ($catId: String!, $skip: Int!, $limit: Int!) {
+//     allWpPost(
+//       filter: { categories: { nodes: { elemMatch: { id: { eq: $catId } } } } }
+//       skip: $skip
+//       limit: $limit
+//     ) {
+//       edges {
+//         node {
+//           id
+//           title
+//           excerpt
+//           uri
+//           slug
+//           date(formatString: "DD MM YYYY")
+//         }
+//       }
+//     }
+//   }
+// `;
